479fd5194ff497bb53c3b0cd951b7e1f1512539a
Michelon Souza <michelon.souza@hotmail.com>
2021-07-16 05:37:48 -0300

    feat: add mockedLocalStorage to setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const mockedLocalStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+  key: jest.fn(),
+  length: 0,
+};
+
+global.localStorage = mockedLocalStorage;
