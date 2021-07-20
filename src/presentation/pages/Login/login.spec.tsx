3b55767c42bf9104e660b7bd11bba030fec3b105
Michelon Souza <michelon.souza@hotmail.com>
2021-07-20 00:30:12 -0300

    test: ensure Login start with inputs has required state

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -7,8 +7,14 @@ describe('LoginPage', () => {
     const { getByTestId } = render(<Login />);
     const errorWrapper = getByTestId('error-wrapper');
     const submitButton = getByTestId('submit') as HTMLButtonElement;
+    const emailStatus = getByTestId('email-status');
+    const passwordStatus = getByTestId('password-status');
 
     expect(errorWrapper.childElementCount).toBe(0);
     expect(submitButton.disabled).toBe(true);
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('🔴');
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('🔴');
   });
 });
