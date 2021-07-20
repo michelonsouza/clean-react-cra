f7bb6de767e984c91d1882505821cbbe49bfe2a6
Michelon Souza <michelon.souza@hotmail.com>
2021-07-19 23:58:51 -0300

    test: esure Login starts with submit button disabled

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -3,10 +3,12 @@ import { render } from '@testing-library/react';
 import { Login } from '.';
 
 describe('LoginPage', () => {
-  it('should not be render spinner and error on application start', () => {
+  it('should start with initial state', () => {
     const { getByTestId } = render(<Login />);
     const errorWrapper = getByTestId('error-wrapper');
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
 
     expect(errorWrapper.childElementCount).toBe(0);
+    expect(submitButton.disabled).toBe(true);
   });
 });
