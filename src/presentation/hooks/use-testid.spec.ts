480dd27caf2639afa2d1ad663e04cb13ea9d9074
Michelon Souza <michelon.souza@hotmail.com>
2021-07-22 21:01:06 -0300

    test: create UseTestId suit tests

---
+++ src/presentation/hooks/use-testid.spec.ts
@@ -0,0 +1,16 @@
+import { useTestId } from '.';
+
+describe('UseTestId', () => {
+  it('should remove data-testid key exists when NODE_ENV is different of "test"', () => {
+    const test = useTestId('test-id');
+
+    expect(Object.keys(test).length).toBe(1);
+  });
+
+  it('should remove data-testid key not exists when NODE_ENV is different of "test"', () => {
+    process.env = { NODE_ENV: 'production' } as any;
+    const test = useTestId('test-id');
+
+    expect(Object.keys(test).length).toBe(0);
+  });
+});
