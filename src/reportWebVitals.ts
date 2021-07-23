743917d5a846887a987e4588c0dbef82171151ba
Michelon Souza <michelon.souza@hotmail.com>
2021-07-23 01:39:04 -0300

    feat: remove ignore paths from package.json and adjust reportWebVitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -2,11 +2,13 @@ import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    const reportAllChanges = process.env.NODE_ENV === 'development';
+
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
+      getCLS(onPerfEntry, reportAllChanges);
+      getFID(onPerfEntry, reportAllChanges);
+      getFCP(onPerfEntry, reportAllChanges);
+      getLCP(onPerfEntry, reportAllChanges);
       getTTFB(onPerfEntry);
     });
   }
