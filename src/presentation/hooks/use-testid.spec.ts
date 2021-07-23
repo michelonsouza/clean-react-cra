27058fbed93d687b1c70e004834cea3852a7ce77
Michelon Souza <michelon.souza@hotmail.com>
2021-07-22 21:09:00 -0300

    refactor: change test name

--- src/presentation/hooks/use-testid.spec.ts
+++ src/presentation/hooks/use-testid.spec.ts
@@ -1,13 +1,13 @@
 import { useTestId } from '.';
 
 describe('UseTestId', () => {
-  it('should remove data-testid key exists when NODE_ENV is different of "test"', () => {
+  it('should data-testid key exists when NODE_ENV is equal to "test"', () => {
     const test = useTestId('test-id');
 
     expect(Object.keys(test).length).toBe(1);
   });
 
-  it('should remove data-testid key not exists when NODE_ENV is different of "test"', () => {
+  it('should remove data-testid key not exists when NODE_ENV is different to "test"', () => {
     process.env = { ...process.env, NODE_ENV: 'production' };
     const test = useTestId('test-id');
 
