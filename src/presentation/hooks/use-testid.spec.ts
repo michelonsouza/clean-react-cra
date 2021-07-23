da1e1539711178da6783390e12962cdd0edca888
Michelon Souza <michelon.souza@hotmail.com>
2021-07-22 21:13:25 -0300

    refactor: move testId value to use faker

--- src/presentation/hooks/use-testid.spec.ts
+++ src/presentation/hooks/use-testid.spec.ts
@@ -1,15 +1,19 @@
+import faker from 'faker';
+
 import { useTestId } from '.';
 
 describe('UseTestId', () => {
   it('should data-testid key exists when NODE_ENV is equal to "test"', () => {
-    const test = useTestId('test-id');
+    const testId = `id-${faker.datatype.uuid()}`;
+    const test = useTestId(testId);
 
     expect(Object.keys(test).length).toBe(1);
+    expect(Object.values(test)[0]).toBe(testId);
   });
 
   it('should remove data-testid key not exists when NODE_ENV is different to "test"', () => {
     process.env = { ...process.env, NODE_ENV: 'production' };
-    const test = useTestId('test-id');
+    const test = useTestId(`id-${faker.datatype.uuid()}`);
 
     expect(Object.keys(test).length).toBe(0);
   });
