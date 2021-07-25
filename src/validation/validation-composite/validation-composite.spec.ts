50ce6c2012c473695e60668e5ad5af8cbdcf6573
Michelon Souza <michelon.souza@hotmail.com>
2021-07-25 01:27:40 -0300

    refactor: move sut creation to a factory function

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -5,23 +5,37 @@ import { FieldValidationSpy } from 'validation/mocks';
 
 import { ValidationComposite } from '.';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldValidationsSpy: FieldValidationSpy[];
+};
+
+function makeSut(fieldName: string): SutTypes {
+  const fieldValidationSpy = new FieldValidationSpy(fieldName);
+  const fieldValidationSpy2 = new FieldValidationSpy(fieldName);
+  const fieldValidationsSpy = [fieldValidationSpy, fieldValidationSpy2];
+  const sut = new ValidationComposite(fieldValidationsSpy);
+
+  return {
+    sut,
+    fieldValidationsSpy,
+  };
+}
+
 describe('ValidationComposite', () => {
   it('should return error if any validation fails', () => {
     const fieldName = faker.database.column();
+    const { sut, fieldValidationsSpy } = makeSut(fieldName);
+    const [fieldValidationSpy, fieldValidationSpy2] = fieldValidationsSpy;
+
     const firstErrorMessage = faker.random.words();
     const secondErrorMessage = faker.random.words();
-    const fieldValidationSpy = new FieldValidationSpy(fieldName);
-    const fieldValidationSpy2 = new FieldValidationSpy(fieldName);
+    const mockedValue = faker.random.words();
 
     fieldValidationSpy.error = new Error(firstErrorMessage);
     fieldValidationSpy2.error = new Error(secondErrorMessage);
 
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2,
-    ]);
-
-    const error = sut.validate(fieldName, 'any_value');
+    const error = sut.validate(fieldName, mockedValue);
 
     expect(error).toBe(firstErrorMessage);
   });
