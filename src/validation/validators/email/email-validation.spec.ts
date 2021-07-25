847d140200afc8489b95f50ac3f94738fe07e95b
Michelon Souza <michelon.souza@hotmail.com>
2021-07-25 00:17:06 -0300

    test: ensure EmailValidation returns falsy if email valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,14 +1,23 @@
-/* eslint-disable max-classes-per-file */
+import faker from 'faker';
+
 import { InvalidFieldError } from 'validation/errors';
 
 import { EmailValidation } from '.';
 
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email');
+    const sut = new EmailValidation(faker.random.word());
 
-    const error = sut.validate('');
+    const error = sut.validate(faker.random.word());
 
     expect(error).toBeInstanceOf(InvalidFieldError);
+  });
+
+  it('should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word());
+
+    const error = sut.validate(faker.internet.email());
+
+    expect(error).toBeFalsy();
   });
 });
